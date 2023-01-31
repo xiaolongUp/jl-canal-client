bb4a74ed7e59a36db4907a0609904b71ccb88e74
xiaolong.yang <18530076638@163.com>
2023-01-31 13:10:18 +0800

    test:测试推送代码

--- src/main/java/com/jielin/canal/JlCanalClientApplication.java
+++ src/main/java/com/jielin/canal/JlCanalClientApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JlCanalClientApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(JlCanalClientApplication.class, args);
     }
